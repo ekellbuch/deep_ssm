bcdc6b36af699e5b06aadf9797b35a0e072f0f1d
E. Kelly Buchanan <3643282+ekellbuch@users.noreply.github.com>
2024-08-14 09:01:24 -0700

    Update example.py
    clean up code not used

--- example.py
+++ example.py
@@ -32,8 +32,6 @@ from einops import rearrange
 import os
 import argparse
 
-#from models.s4.s4 import S4Block as S4  # Can use full version instead of minimal S4D standalone below
-#from models.s4.s4d import S4D
 from deep_ssm.models.s5_fjax.ssm import S5
 from tqdm.auto import tqdm
 from typing import Literal, Tuple, Optional, Union
@@ -220,9 +218,9 @@ class S5Model(nn.Module):
 
 def setup_optimizer(model, lr_factor, ssm_lr_base, weight_decay, epochs, steps_per_epoch):
   """
-  S4 requires a specific optimizer setup.
+  S5 requires a specific optimizer setup.
 
-  The S4 layer (A, B, C, dt) parameters typically
+  The S5 layer (A, B, C, dt) parameters typically
   require a smaller learning rate (typically 0.001), with no weight decay.
 
   The rest of the model can be trained with a higher learning rate (e.g. 0.004, 0.01)
@@ -266,52 +264,6 @@ def setup_optimizer(model, lr_factor, ssm_lr_base, weight_decay, epochs, steps_p
 
   return optimizer, scheduler
 
-
-def setup_optimizer_s4(model, lr, weight_decay, epochs):
-  """
-  S4 requires a specific optimizer setup.
-
-  The S4 layer (A, B, C, dt) parameters typically
-  require a smaller learning rate (typically 0.001), with no weight decay.
-
-  The rest of the model can be trained with a higher learning rate (e.g. 0.004, 0.01)
-  and weight decay (if desired).
-  """
-
-  # All parameters in the model
-  all_parameters = list(model.parameters())
-
-  # General parameters don't contain the special _optim key
-  params = [p for p in all_parameters if not hasattr(p, "_optim")]
-
-  # Create an optimizer with the general parameters
-  optimizer = optim.AdamW(params, lr=lr, weight_decay=weight_decay)
-
-  # Add parameters with special hyperparameters
-  hps = [getattr(p, "_optim") for p in all_parameters if hasattr(p, "_optim")]
-  hps = [
-    dict(s) for s in sorted(list(dict.fromkeys(frozenset(hp.items()) for hp in hps)))
-  ]  # Unique dicts
-  for hp in hps:
-    params = [p for p in all_parameters if getattr(p, "_optim", None) == hp]
-    optimizer.add_param_group(
-      {"params": params, **hp}
-    )
-
-  # Create a lr scheduler
-  # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=patience, factor=0.2)
-  scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
-
-  # Print optimizer info
-  keys = sorted(set([k for hp in hps for k in hp.keys()]))
-  for i, g in enumerate(optimizer.param_groups):
-    group_hps = {k: g.get(k, None) for k in keys}
-    print(' | '.join([
-                       f"Optimizer group {i}",
-                       f"{len(g['params'])} tensors",
-                     ] + [f"{k} {v}" for k, v in group_hps.items()]))
-
-  return optimizer, scheduler
 
 
 ###############################################################################
@@ -457,7 +409,7 @@ if __name__ == "__main__":
               transforms.Lambda(view_transform)
           ])
 
-      # S4 is trained on sequences with no data augmentation!
+      # S5 is trained on sequences with no data augmentation!
       transform_train = transform_test = transform
 
       trainset = torchvision.datasets.CIFAR10(
