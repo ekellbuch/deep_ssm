af6151aa2019fcc4b13cc752954f4b98858faf07
Kelly Buchanan <ekellbuchanan@gmail.com>
2024-10-10 03:40:46 -0700

    fix callback

--- src/deep_ssm/utils/callbacks.py
+++ src/deep_ssm/utils/callbacks.py
@@ -4,7 +4,7 @@ from lightning.pytorch.utilities import grad_norm
 
 class GradNormCallback_vars_pbatch(Callback):
     """
-    Logs the gradient norm for each parameter individually.
+    Logs gradnorm in batch
     """
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         #TODO:time grad_norm call vs detach().data.norm(2):
@@ -15,36 +15,26 @@ class GradNormCallback_vars_pbatch(Callback):
 
 class GradNormCallback_vars_pepoch(Callback):
     """
-    Logs the gradient norm for each parameter individually.
+    Logs gradnorm in epoch
     """
-
     def on_train_epoch_end(self, trainer, pl_module):
-        for name, param in pl_module.named_parameters():
-            if param.grad is not None:
-                # Calculate the norm of the gradient
-                param_grad_norm = param.grad.detach().data.norm(2).item()
-                # Log the gradient norm for this parameter
-                pl_module.log(f"grad_norm/{name}", param_grad_norm)
-
-        # Optionally, log the total gradient norm
-        total_grad_norm = gradient_norm(pl_module)
-        pl_module.log("grad_norm/model", total_grad_norm)
-
+        norms = grad_norm(pl_module.model, norm_type=2)  # You can change `norm_type` to 1 for L1 norm
+        for layer_name, norm_value in norms.items():
+            self.log(f'grad_norm_{layer_name}', norm_value, on_step=True, on_epoch=False)
 
 
 class GradNormCallback(Callback):
     """
-  Logs the gradient norm.
+    Logs gradnorm at the end of the backward pass
   Edited from https://github.com/Lightning-AI/lightning/issues/1462
   """
-
     def on_after_backward(self, trainer, pl_module):
         pl_module.log("grad_norm/model", gradient_norm(pl_module))
 
 
 class GradNormCallback_pepoch(Callback):
     """
-    Logs the gradient norm.
+    Logs gradnorm at the end of the epoch
     Edited from https://github.com/Lightning-AI/lightning/issues/1462
     """
 
