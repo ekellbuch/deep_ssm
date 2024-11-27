4049e4cd2138e38ade8a0a0e19a51aa466b5ca37
Alex Wang <11478740+KeAWang@users.noreply.github.com>
2024-11-26 16:05:44 -0800

    add tcn baseline

--- src/deep_ssm/models/__init__.py
+++ src/deep_ssm/models/__init__.py
@@ -1,7 +1,8 @@
-from .bci_models import MambaDecoder, GRUDecoder, SashimiDecoder
+from .bci_models import MambaDecoder, GRUDecoder, SashimiDecoder, TCNDecoder
 
 all_models = {
   "bci_gru" : GRUDecoder,
+  "bci_tcn" : TCNDecoder,
   "bci_mamba": MambaDecoder,
   "bci_sashimi": SashimiDecoder
 }
--- src/deep_ssm/models/bci_models.py
+++ src/deep_ssm/models/bci_models.py
@@ -197,6 +197,136 @@ class GRUDecoder(BaseDecoder):
         return seq_out
 
 
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+
+
+class TemporalConvNet(nn.Module):
+    def __init__(self, num_inputs, channel_sizes, kernel_size=2, dropout=0.2):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_blocks = len(channel_sizes)
+        for i in range(num_blocks):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else channel_sizes[i-1]
+            out_channels = channel_sizes[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+class TCN(nn.Module):
+    def __init__(self, input_size, channel_sizes, output_size, kernel_size, dropout, channel_last=False):
+        super(TCN, self).__init__()
+        self.tcn = TemporalConvNet(input_size, channel_sizes, kernel_size=kernel_size, dropout=dropout)
+        #self.linear = nn.Linear(channel_sizes[-1], output_size)
+        self.channel_last = channel_last
+
+    def forward(self, x):
+        if self.channel_last:
+            x = x.transpose(-1, -2)
+        y1 = self.tcn(x)
+        if self.channel_last:
+            y1 = y1.transpose(-1, -2)
+        return y1
+
+
+class TemporalBlock(nn.Module):
+    def __init__(self, input_size, output_size, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = nn.Conv1d(input_size, output_size, kernel_size, stride=stride, padding=padding, dilation=dilation)
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = nn.Conv1d(output_size, 2 * output_size, kernel_size, stride=stride, padding=padding, dilation=dilation)
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.glu = nn.GLU(dim=-2)
+
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2, self.glu)
+
+        self.downsample = nn.Conv1d(input_size, output_size, 1) if input_size != output_size else None
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return out + res
+
+
+class TCNDecoder(BaseDecoder):
+    def __init__(
+        self,
+        neural_dim,
+        n_classes,
+        hidden_dim,
+        layer_dim,
+        kernel_size,
+        dropout,
+        *,
+        nDays=24,
+        strideLen=4,
+        kernelLen=14,
+        gaussianSmoothWidth=0,
+        unfolding=True,
+        input_nonlinearity="softsign",
+    ):
+        super().__init__(
+            neural_dim=neural_dim,
+            nDays=nDays,
+            strideLen=strideLen,
+            kernelLen=kernelLen,
+            gaussianSmoothWidth=gaussianSmoothWidth,
+            unfolding=unfolding,
+            input_nonlinearity=input_nonlinearity,
+        )
+
+        self.layer_dim = layer_dim
+        self.hidden_dim = hidden_dim
+        self.dropout = dropout
+
+        if unfolding:
+          input_dims = self.neural_dim * kernelLen
+        else:
+          input_dims = self.neural_dim
+
+        self.input_dims = input_dims
+
+        self.tcn = TCN(
+            input_size=input_dims,
+            channel_sizes=[hidden_dim] * layer_dim,
+            output_size=hidden_dim,
+            kernel_size=kernel_size,
+            dropout=dropout,
+            channel_last=True,
+        )
+
+        self.fc_decoder_out = nn.Linear(
+            self.hidden_dim, n_classes + 1
+        )  # +1 for CTC blank
+
+    def forward(self, neuralInput, dayIdx):
+        # Preprocess the input (e.g., Gaussian smoothing and unfolding)
+        stridedInputs = self.forward_preprocessing(neuralInput, dayIdx)
+
+        hid = self.tcn(stridedInputs)        
+
+        # Apply Decoder
+        seq_out = self.fc_decoder_out(hid)
+        return seq_out
+
+
 class MambaDecoder(BaseDecoder):
     def __init__(
         self,
