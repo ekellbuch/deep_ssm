577ba1a83ac0bac41e042d5db716c4208f3857cc
Xavier Gonzalez <xavier18@stanford.edu>
2024-12-10 13:59:12 -0800

    Start getting ready to prototype on sequential mnist

---
+++ experiments/proto_seq_mnist_Xaviers-MacBook-Pro.local_Dec-10-134841-2024_Conflict/seq_mnist.py
@@ -0,0 +1,7 @@
+"""
+Goal is to build an extremely lightweight script for prototyping models using sequential mnist.
+
+TODO:
+* Can I apply the prnn object to sequential mnist
+* Can I modify prnn to have a learnable diagonal Jacobian instead of one that comes from nonlinear dynamics?
+"""
---
+++ src/deep_ssm/models/time_series_classifiers.py
@@ -0,0 +1,4 @@
+"""
+time_series_classifiers.py
+Build some models to do time series classification, building off of prnn
+"""
